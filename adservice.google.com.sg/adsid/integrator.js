014201013c2162d3fbf01be33983fe8db6dbb8c8
czdm <108321992+czdm@users.noreply.github.com>
2022-06-29 01:07:46 +0800

    0629

---
+++ adservice.google.com.sg/adsid/integrator.js
@@ -0,0 +1,7 @@
+processGoogleToken({
+    "newToken": "ChEI8M3qlQYQ7bKo1N732oL2ARI4AMYediJX3hK3HNLCQzhWPo5x_PNvM9_MyO_wkwB4utwyvrcBuIBGB0eG1SMPEtMuHjP0VZvPdbE",
+    "validLifetimeSecs": 300,
+    "freshLifetimeSecs": 300,
+    "1p_jar": "",
+    "pucrd": ""
+});
---
+++ adservice.google.com/adsid/integrator.js
@@ -0,0 +1,7 @@
+processGoogleToken({
+    "newToken": "ChEI8M3qlQYQ7bKo1N732oL2ARI4AMYediLz5viEbbZ4FRsP8A55PKHf8R9LLiZSn_GPWznKiiwiuLjtvNd9kLDK9QjKWUQt0OsDD-A",
+    "validLifetimeSecs": 300,
+    "freshLifetimeSecs": 300,
+    "1p_jar": "",
+    "pucrd": ""
+});
